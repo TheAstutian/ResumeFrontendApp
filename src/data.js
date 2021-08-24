1e7eb58acd138eae8d69e0178aae51f4d3164ae2
TheAstutian <speakingatoms@gmail.com>
2021-08-24 10:06:04 +0100

    added crwn clothing to portfolio

--- src/data.js
+++ src/data.js
@@ -1,12 +1,28 @@
 export const projects = [
-    {
-      title: "Wese 90",
-      subtitle: "HTML & CSS",
-      description:
-        "A simple website for a delivery chain enterprise",
-      image: "./wese90.png",
-      link: "https://wese90.netlify.app/",
-    },
+  {
+    title: "Crown Clothing",
+    subtitle: "React and Firebase",
+    description:
+      "An e-commerce/clothes shopping website. Users can create accounts, log in and make purchases.",
+    image: "./crwn-clothing.png",
+    link: "https://crwn-live-lqt.herokuapp.com/",
+  },
+  {
+    title: "DigiCeipt",
+    subtitle: "Python Django",
+    description:
+      "An app that generates digital receipts for online vendors",
+    image: "./digiceipt.png",
+    link: "https://digiceiptbe.herokuapp.com/",
+  },
+  {
+    title: "The Astutian",
+    subtitle: "HTM & CSS",
+    description:
+      "A simple website for a local enterprise",
+    image: "./astutian.png",
+    link: "https://theastutian.github.io/",
+  },
     {
       title: "Epic Todo App",
       subtitle: "React and Django",
@@ -15,21 +31,14 @@ export const projects = [
       image: "./todoapp.gif",
       link: "https://github.com/TheAstutian/todo-django-react",
     },
+    
     {
-      title: "The Astutian",
-      subtitle: "React and Firebase",
+      title: "Wese 90",
+      subtitle: "HTML & CSS",
       description:
-        "A simple website for a local enterprise",
-      image: "./astutian.png",
-      link: "https://theastutian.github.io/",
-    },
-    {
-      title: "DigiCeipt",
-      subtitle: "Python Django",
-      description:
-        "An app that generates digital receipts for online vendors",
-      image: "./digiceipt.png",
-      link: "https://digiceiptbe.herokuapp.com/",
+        "A simple website for a delivery chain enterprise",
+      image: "./wese90.png",
+      link: "https://wese90.netlify.app/",
     },
   ];
 
